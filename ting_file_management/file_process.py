d60f389f5b51f4d722be778708c066982d64f169
Victor Mendes <victorutdsim7@gmail.com>
2023-06-06 16:45:34 -0300

    feat: req 3 -> implementação função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+import sys
+
+
+def queue_already_has_file(path_file, queue):
+    for file in queue:
+        if file["nome_do_arquivo"] == path_file:
+            return True
+    return False
+
+
+def process(path_file: str, instance: Queue):
+    if not queue_already_has_file(path_file, instance.queue):
+        lines = txt_importer(path_file)
+        file_info = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines,
+        }
+
+        instance.enqueue(file_info)
+        sys.stdout.write(str(file_info))
 
 
 def remove(instance):
