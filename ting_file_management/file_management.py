81f70012733be947e9480a070fe6cad835525680
Victor Mendes <victorutdsim7@gmail.com>
2023-06-06 15:16:02 -0300

    feat: req 2 -> implementação importador de txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,24 @@
-def txt_importer(path_file):
-    ...
+import sys
+import pathlib
+
+
+def file_extension_is_valid(file):
+    suffix = pathlib.Path(file).suffix
+    available_extensions = {".txt"}
+
+    if suffix not in available_extensions:
+        return False
+
+    return True
+
+
+def txt_importer(path_file: str):
+    if not file_extension_is_valid(path_file):
+        return sys.stderr.write("Formato inválido")
+
+    try:
+        with open(path_file) as file:
+            lines = [line.strip() for line in file.readlines()]
+            return lines
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
